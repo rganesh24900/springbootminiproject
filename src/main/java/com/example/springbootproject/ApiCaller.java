b7915900c1804e6da19bd44bbb64097d99b2eb60
Ganesh <rganesh802@gmail.com>
2021-11-18 21:00:18 +0530

    added @values annotation

--- src/main/java/com/example/springbootproject/ApiCaller.java
+++ src/main/java/com/example/springbootproject/ApiCaller.java
@@ -1,6 +1,7 @@
 package com.example.springbootproject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,19 +16,21 @@ public class ApiCaller {
     @Autowired
     RestTemplate restTemplate;
     HttpHeaders httpHeaders = new HttpHeaders();
+    @Value("${apicaller.api.url}")
+    public String url;
 
     public String postApiCaller(Entities a) {
         httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<Entities> httpEntity = new HttpEntity<>(a, httpHeaders);
 
 
-        return restTemplate.exchange("http://localhost:8080/bloodgroups", HttpMethod.POST, httpEntity, String.class).getBody();
+        return restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class).getBody();
     }
 
     public String getApiCaller(Entities b) {
         httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<Entities> httpEntity = new HttpEntity<>(b, httpHeaders);
 
-        return restTemplate.exchange("http://localhost:8080/bloodgroups", HttpMethod.GET, httpEntity, String.class).getBody();
+        return restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class).getBody();
     }
 }
